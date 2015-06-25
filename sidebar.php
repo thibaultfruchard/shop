7b48c52190442cb0fb987eefc7703fef8c94ebd7
Webforce3 <denis.nerborac@wf3.fr>
2015-06-25 15:09:20 +0200

    Update sidebar - Add random products query + use displayProduct()

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,8 @@
+<?php
+
+$random_products = $db->query('SELECT * FROM products ORDER BY RAND() LIMIT 2')->fetchAll();
+
+?>
             <div class="col-md-3">
 
                 <p class="lead">Categories</p>
@@ -11,56 +16,10 @@
 
                 <p class="lead">Featured products</p>
 
-                <div class="product">
-                    <div class="thumbnail">
-                        <img src="http://placehold.it/320x150" alt="">
-                        <div class="caption">
-                            <h4 class="pull-right">24.99 €</h4>
-                            <h4><a href="#">First Product</a>
-                            </h4>
-                            <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </div>
-                        <div class="ratings">
-                            <p class="pull-right">12 reviews</p>
-                            <p>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star-empty"></span>
-                            </p>
-                        </div>
-                        <div class="btns clearfix">
-                            <a class="btn btn-info pull-left" href=""><span class="glyphicon glyphicon-eye-open"></span> View</a>
-                            <a class="btn btn-primary pull-right" href=""><span class="glyphicon glyphicon-shopping-cart"></span> Add to cart</a>
-                        </div>
-                    </div><!-- /.thumbnail -->
-                </div><!-- /.product -->
-
-                <div class="product">
-                    <div class="thumbnail">
-                        <img src="http://placehold.it/320x150" alt="">
-                        <div class="caption">
-                            <h4 class="pull-right">64.99 €</h4>
-                            <h4><a href="#">Second Product</a>
-                            </h4>
-                            <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </div>
-                        <div class="ratings">
-                            <p class="pull-right">12 reviews</p>
-                            <p>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star"></span>
-                                <span class="glyphicon glyphicon-star-empty"></span>
-                            </p>
-                        </div>
-                        <div class="btns clearfix">
-                            <a class="btn btn-info pull-left" href=""><span class="glyphicon glyphicon-eye-open"></span> View</a>
-                            <a class="btn btn-primary pull-right" href=""><span class="glyphicon glyphicon-shopping-cart"></span> Add to cart</a>
-                        </div>
-                    </div><!-- /.thumbnail -->
-                </div><!-- /.product -->
+                <?php
+                foreach($random_products as $product) {
+                    echo displayProduct($product);
+                }
+                ?>
 
             </div><!-- /.col-md-3 -->
