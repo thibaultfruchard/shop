adee04b0644537629c627504d47b34bcc1f4a3a1
Webforce3 <denis.nerborac@wf3.fr>
2015-06-25 12:20:06 +0200

    Update homepage - Add 6 last products query + display

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php include_once 'header.php' ?>
+<?php
+include_once 'header.php';
+
+$last_products = $db->query('SELECT * FROM products ORDER BY date DESC LIMIT 6')->fetchAll();
+
+?>
 
         <div class="row">
 
@@ -47,14 +52,15 @@
 
                 <div class="row">
 
+                    <?php foreach($last_products as $product) { ?>
                     <div class="product col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
                             <img src="http://placehold.it/320x150" alt="">
                             <div class="caption">
-                                <h4 class="pull-right">24.99 €</h4>
-                                <h4><a href="#">First Product</a>
+                                <h4 class="pull-right"><?= $product['price'] ?> €</h4>
+                                <h4><a href="#"><?= $product['name'] ?></a>
                                 </h4>
-                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                <p><?= substr($product['description'], 0, 50) ?></p>
                             </div>
                             <div class="ratings">
                                 <p class="pull-right">15 reviews</p>
@@ -72,136 +78,7 @@
                             </div>
                         </div><!-- /.thumbnail -->
                     </div><!-- /.product -->
-
-                    <div class="product col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
-                            <div class="caption">
-                                <h4 class="pull-right">64.99 €</h4>
-                                <h4><a href="#">Second Product</a>
-                                </h4>
-                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                            <div class="ratings">
-                                <p class="pull-right">12 reviews</p>
-                                <p>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star-empty"></span>
-                                </p>
-                            </div>
-                            <div class="btns clearfix">
-                                <a class="btn btn-info pull-left" href="product.html"><span class="glyphicon glyphicon-eye-open"></span> View</a>
-                                <a class="btn btn-primary pull-right" href="product.html"><span class="glyphicon glyphicon-shopping-cart"></span> Add to cart</a>
-                            </div>
-                        </div><!-- /.thumbnail -->
-                    </div><!-- /.product -->
-
-                    <div class="product col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
-                            <div class="caption">
-                                <h4 class="pull-right">74.99 €</h4>
-                                <h4><a href="#">Third Product</a>
-                                </h4>
-                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                            <div class="ratings">
-                                <p class="pull-right">31 reviews</p>
-                                <p>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star-empty"></span>
-                                </p>
-                            </div>
-                            <div class="btns clearfix">
-                                <a class="btn btn-info pull-left" href="product.html"><span class="glyphicon glyphicon-eye-open"></span> View</a>
-                                <a class="btn btn-primary pull-right" href="product.html"><span class="glyphicon glyphicon-shopping-cart"></span> Add to cart</a>
-                            </div>
-                        </div><!-- /.thumbnail -->
-                    </div><!-- /.product -->
-
-                    <div class="product col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
-                            <div class="caption">
-                                <h4 class="pull-right">84.99 €</h4>
-                                <h4><a href="#">Fourth Product</a>
-                                </h4>
-                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                            <div class="ratings">
-                                <p class="pull-right">6 reviews</p>
-                                <p>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star-empty"></span>
-                                    <span class="glyphicon glyphicon-star-empty"></span>
-                                </p>
-                            </div>
-                            <div class="btns clearfix">
-                                <a class="btn btn-info pull-left" href="product.html"><span class="glyphicon glyphicon-eye-open"></span> View</a>
-                                <a class="btn btn-primary pull-right" href="product.html"><span class="glyphicon glyphicon-shopping-cart"></span> Add to cart</a>
-                            </div>
-                        </div><!-- /.thumbnail -->
-                    </div><!-- /.product -->
-
-                    <div class="product col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
-                            <div class="caption">
-                                <h4 class="pull-right">94.99 €</h4>
-                                <h4><a href="#">Fifth Product</a>
-                                </h4>
-                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                            <div class="ratings">
-                                <p class="pull-right">18 reviews</p>
-                                <p>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star-empty"></span>
-                                </p>
-                            </div>
-                            <div class="btns clearfix">
-                                <a class="btn btn-info pull-left" href="product.html"><span class="glyphicon glyphicon-eye-open"></span> View</a>
-                                <a class="btn btn-primary pull-right" href="product.html"><span class="glyphicon glyphicon-shopping-cart"></span> Add to cart</a>
-                            </div>
-                        </div><!-- /.thumbnail -->
-                    </div><!-- /.product -->
-
-                    <div class="product col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
-                            <div class="caption">
-                                <h4 class="pull-right">94.99 €</h4>
-                                <h4><a href="#">Sixth Product</a>
-                                </h4>
-                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                            <div class="ratings">
-                                <p class="pull-right">18 reviews</p>
-                                <p>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star"></span>
-                                    <span class="glyphicon glyphicon-star-empty"></span>
-                                </p>
-                            </div>
-                            <div class="btns clearfix">
-                                <a class="btn btn-info pull-left" href="product.html"><span class="glyphicon glyphicon-eye-open"></span> View</a>
-                                <a class="btn btn-primary pull-right" href="product.html"><span class="glyphicon glyphicon-shopping-cart"></span> Add to cart</a>
-                            </div>
-                        </div><!-- /.thumbnail -->
-                    </div><!-- /.product -->
+                    <?php } ?>
 
                 </div><!-- /.row -->
 
